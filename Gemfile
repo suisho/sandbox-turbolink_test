29b87d1545a05309c23071a0c6957608edf93f1a
suisho <suisho000000@gmail.com>
2014-07-15 00:43:06 +0900

    pjax

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+source 'https://rails-assets.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -18,7 +19,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+# gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
@@ -42,3 +43,5 @@ gem 'spring',        group: :development
 gem "bourbon"
 gem "pry-rails"
 #gem 'rails-assets-jquery'
+gem 'rails-assets-jquery-pjax'
+gem "pjax-rails"
--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,14 +1,32 @@
 $(function(){
   $(".foo").addClass("animation")
-  $(".next").on("click", function(e){
+  /*$(".next").on("click", function(e){
     var url = $(this).attr("href")
     $(".foo").on("transitionend", function(e){
-      Turbolinks.visit(url)
+      //Turbolinks.visit(url)
     }).addClass("animation")
-    return false;
-  })
+    //return false;
+  })*/
 })
 
 $(document).on("ready page:load page:restore", function(){
   $(".foo").removeClass("animation")
+})
+/*$(document).on("ready", function(){
+  $(document).pjax("a.next", "#container")
+})*/
+
+$(function(){
+  $(document).on("pjax:end", function(){
+    console.log("aa")
+    $(".foo").removeClass("animation")
+  })
+  $("a.next").on("click", function(e){
+    $(".foo").one("transitionend", function(){
+      $.pjax({url : $("a.next").attr("href"), 
+        container : "#container"
+      })
+    }).addClass("animation")
+    return false;
+  })
 })
